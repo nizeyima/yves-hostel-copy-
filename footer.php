16621d5f338ae89fd54fa6bb0d99c4a197890bad
irakozeyvette <yvetteirakoze9@gmail.com>
2023-04-21 13:54:18 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer style="text-align:center; background-color: black; color: white; position: fixed; bottom: 0; width: 100%;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h4>Contact Us</h4>
+        <p>Bugema University<br>P.O Box 6529 Kampala, Uganda<br>Tel: +256-414-699 557<br>Email: info@bugemauniv.ac.ug</p>
+      </div>
+    </div>
+  </div>
+</footer>
